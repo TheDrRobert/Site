af202fa76081187c3522d5add545820474cff993
TheDrRobert <robertdwow@gmail.com>
2017-10-07 12:34:39 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,516 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style class="silex-inline-styles" type="text/css">
+        .body-initial {
+            background-image: url('../master/cg70Zbd.jpg');
+            background-position: center center;
+            background-attachment: fixed;
+            background-repeat: repeat;
+            background-size: auto;
+        }
+        
+        @media only screen and (max-width: 480px),
+        only screen and (max-device-width: 480px) {
+            .body-initial {
+                background-attachment: fixed;
+                background-size: auto;
+            }
+        }
+        
+        .silex-id-1506913681926-0 {
+            width: 260px;
+            min-height: 76px;
+            top: 9px;
+            left: 30px;
+            background-color: transparent;
+            opacity: 1;
+        }
+        
+        @media only screen and (max-width: 480px),
+        only screen and (max-device-width: 480px) {
+            .silex-id-1506913681926-0 {
+                left: 68px;
+                top: 60px;
+                width: 260px;
+                min-height: 36px;
+            }
+        }
+        
+        .silex-id-1506914537622-2 {
+            width: 276px;
+            min-height: 461px;
+            top: 43px;
+            left: 19px;
+        }
+        
+        @media only screen and (max-width: 480px),
+        only screen and (max-device-width: 480px) {
+            .silex-id-1506914537622-2 {
+                left: 185px;
+                top: 778px;
+                width: 276px;
+                min-height: 394px;
+            }
+        }
+        
+        .silex-id-1507392459628-0 {
+            width: 400px;
+            height: 400px;
+            background-color: transparent;
+            top: 32px;
+            left: 291px;
+        }
+        
+        @media only screen and (max-width: 480px),
+        only screen and (max-device-width: 480px) {
+            .silex-id-1507392459628-0 {
+                top: 590px;
+                left: 84px;
+                width: 377px;
+                height: 291px;
+            }
+        }
+    </style>
+    <style type="text/css" class="silex-style"></style>
+    <script type="text/javascript" class="silex-script"></script>
+    <title>The Dr. Robert</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.2" data-silex-viewport="">
+
+
+    <style type="text/css" class="silex-style-settings">
+        .website-width {
+            width: 1200px;
+        }
+    </style>
+
+
+
+    <meta name="description" content="The Dr. Robert">
+    <link href="../../../../../../../Icon.jpg" rel="shortcut icon">
+
+
+
+    <style id="current-page-style">
+        .page-page-1 {
+            display: inherit;
+        }
+    </style>
+    <!-- Silex HEAD tag do not remove -->
+    <!-- End of Silex HEAD tag do not remove -->
+</head>
+
+<body data-silex-id="body-initial" class="body-initial enable-mobile silex-runtime" data-silex-type="container">
+    <div class="silex-pages">
+
+
+        <a id="page-page-1" data-silex-type="page" class="page-element page-link-active">Page 1</a></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <base class="silex-temp-tag" href="../master/test.html" target="_blank">
+    <meta charset="UTF-8">
+    <!-- generator meta tag -->
+    <!-- leave this for stats and Silex version check -->
+    <meta name="generator" content="Silex v2.2.7">
+    <!-- End of generator meta tag -->
+    <script type="text/javascript" src="//editor.silex.me/static/2.7/jquery.js" data-silex-static=""></script>
+    <script type="text/javascript" src="//editor.silex.me/static/2.7/jquery-ui.js" data-silex-static=""></script>
+    <script type="text/javascript" src="//editor.silex.me/static/2.7/pageable.js" data-silex-static=""></script>
+    <script type="text/javascript" src="//editor.silex.me/static/2.7/front-end.js" data-silex-static=""></script>
+    <link rel="stylesheet" href="//editor.silex.me/static/2.7/normalize.css" data-silex-static="">
+    <link rel="stylesheet" href="//editor.silex.me/static/2.7/front-end.css" data-silex-static="">
+
+    <style type="text/css" class="silex-style">
+        /*
+
+	Írta: Kálmán Roland (Pradox)
+	Kapcsolat: pradoxblog@gmail.com (email), pradox@index.hu (msn)
+	Dátum: 2010.07.25.
+
+*/
+        
+        body {
+            background-image: url('../../img/background.jpg');
+            background-position: top center;
+            margin: 0px;
+            text-shadow: #000 0 0 3px;
+        }
+        
+        img {
+            border: 0;
+        }
+        
+        table.reg {
+            width: 600px;
+            margin: 40px 0 0 90px;
+        }
+        
+        div.copy {
+            color: #ccc;
+            font-size: 12px;
+            font-family: arial;
+            text-align: center;
+        }
+        
+        div.errors {
+            color: #fff;
+            font-family: arial;
+            font-size: 12px;
+            font-weight: bold;
+            text-align: center;
+        }
+        
+        table.form {
+            width: 100%;
+            color: #fff;
+            margin: 15px 0;
+            font-family: arial;
+            border-spacing: 5px;
+            font-size: 16px;
+        }
+        
+        input,
+        select {
+            width: 200px;
+            padding: 3px;
+            background-color: #f7f7f7;
+            color: #454545;
+            border: 0;
+            font-size: 14px;
+            -moz-border-radius: 3px;
+            -webkit-border-radius: 3px;
+            border-radius: 3px;
+            box-shadow: 0px 0px 3px #000;
+            -moz-box-shadow: 0px 0px 3px #000;
+            -webkit-box-shadow: 0px 0px 3px #000;
+        }
+        
+        input:focus,
+        select:focus {
+            box-shadow: 0px 0px 3px #fff;
+            -moz-box-shadow: 0px 0px 3px #fff;
+            -webkit-box-shadow: 0px 0px 3px #fff;
+            color: #1b1b1b;
+        }
+        
+        input.sbm {
+            width: auto;
+            padding: 3px 15px;
+            background-image: url('../../img/inputbg.png');
+            color: #dfdfdf;
+            border: 0;
+            font-size: 14px;
+            -moz-border-radius: 3px;
+            margin-top: 30px;
+            -webkit-border-radius: 3px;
+            border-radius: 3px;
+            box-shadow: 0px 0px 1px #ccc;
+            -moz-box-shadow: 0px 0px 1px #ccc;
+            -webkit-box-shadow: 0px 0px 1px #ccc;
+        }
+        
+        input.sbm:hover {
+            box-shadow: 0px 0px 3px #fff;
+            -moz-box-shadow: 0px 0px 3px #fff;
+            -webkit-box-shadow: 0px 0px 3px #fff;
+            color: #fff;
+            background-image: url('../../img/inputbg2.png');
+        }
+    </style>
+    <script type="text/javascript" class="silex-script"></script>
+    <style class="silex-inline-styles" type="text/css">
+        .body-initial {
+            background-color: rgba(255, 255, 255, 1);
+        }
+        
+        .silex-id-1478366450713-3 {
+            top: 100px;
+            left: NaNpx;
+            background-color: transparent;
+        }
+        
+        .silex-id-1478366450713-2 {
+            min-height: 100px;
+            background-color: transparent;
+            width: 1200px;
+        }
+        
+        .silex-id-1478366444112-1 {
+            top: 100px;
+            left: NaNpx;
+            background-color: transparent;
+            background-attachment: fixed;
+        }
+        
+        .silex-id-1478366444112-0 {
+            min-height: 100px;
+            background-color: transparent;
+            width: 1200px;
+        }
+    </style>
+    <title></title>
+    <script type="text/javascript" class="silex-json-styles">
+        [{"desktop":{"body-initial":{"background-image":"url('../master/cg70Zbd.jpg')","background-position":"center center","background-attachment":"fixed","background-repeat":"repeat","background-size":"auto"},"silex-id-1506913681926-0":{"width":"260px","min-height":"76px","top":"9px","left":"30px","background-color":"transparent","opacity":"1"},"silex-id-1506914537622-2":{"width":"276px","min-height":"461px","top":"43px","left":"19px"},"silex-id-1507392459628-0":{"width":"400px","height":"400px","background-color":"transparent","top":"32px","left":"291px"}},"mobile":{"silex-id-1506913681926-0":{"left":"68px","top":"60px","width":"260px","min-height":"36px"},"silex-id-1506914537622-2":{"left":"185px","top":"778px","width":"276px","min-height":"394px"},"body-initial":{"background-attachment":"fixed","background-size":"auto","background-image":""},"silex-id-1507392459628-0":{"top":"493px","left":"10px","width":"377px","height":"291px"}},"componentData":{"silex-id-1507392459628-0":{"name":"form1","templateName":"form","sendTo":"robertdwow@gmail.com","field1":true,"labelTextColor":"#c0c0c0","placeholder2":"you@email.com","placeholder3":"What do you have to say?"}}}]
+    </script>
+
+
+
+
+
+
+    <meta name="website-width" content="1200">
+    <style type="text/css" class="silex-style-settings">
+        .website-width {
+            width: 1200px;
+        }
+    </style>
+
+
+
+
+
+
+
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.2" data-silex-viewport="">
+
+
+
+
+
+
+
+    <div data-silex-type="text" class="editable-style silex-id-1506913681926-0 text-element" data-silex-id="silex-id-1506913681926-0" style="">
+        <div class="silex-element-content normal">
+            <div style="text-align: center;"><span style="color: rgb(255, 255, 255); font-family: Roboto; font-size: xx-large;">The Dr. Robert</span></div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <div data-silex-type="text" class="editable-style silex-id-1506914537622-2 text-element" data-silex-id="silex-id-1506914537622-2" style="">
+        <div class="silex-element-content normal">
+            <div style="text-align: center;"><span style="color: rgb(241, 194, 50); font-size: x-large;">Social:</span></div>
+            <div style="font-size: large; text-align: center;">
+                <font color="#cccccc">[<a href="https://www.facebook.com/TheDrRobert.2" style="">Facebook
+                </a>
+                </font>
+            </div>
+
+            <div style="text-align: center;">
+
+                <font color="#cccccc">
+                    <a href="https://twitter.com/The_Dr_Robert">
+                        <font size="4">Twitter</font>
+                    </a>
+
+                </font>
+            </div>
+            <div style="text-align: center;">
+
+                <font color="#cccccc">
+                    <a href="https://plus.google.com/+EarthlyF0rg0tenTheDrRobert">
+                        <font size="4">Google+</font>
+                    </a>
+
+                </font>
+            </div>
+            <div style="text-align: center;">
+                <font color="#cccccc">
+                    <a href="https://www.reddit.com/user/The_Dr_Robert/" style="outline: 0px;">
+                        <font size="4">Reddit</font>
+                    </a>
+                </font>
+            </div>
+            <div style="text-align: center;">
+                <font style="outline: 0px;" size="4" color="#cccccc"><a href="https://www.linkedin.com/in/robert-dillard-08b0b5a0" style="outline: 0px;">LinkedIn</a>]</font>
+            </div>
+            <div style="text-align: center;">
+                <font size="5" color="#f1c232" style="">Videos:</font>
+            </div>
+            <div style="text-align: center;">
+                <font color="#cccccc" style="outline: 0px; font-size: large;">[<a href="https://www.youtube.com/channel/UC8ZWNc6QX40073dLr3KwLLQ" style="outline: 0px; font-size: large;">YouTube</a>]</font>
+                <a href="https://www.youtube.com/channel/UC8ZWNc6QX40073dLr3KwLLQ"></a>
+            </div>
+            <div style="text-align: center;">
+                <font size="5" color="#f1c232" style="">Projects:</font>
+            </div>
+            <div style="text-align: center;">
+
+                <font color="#cccccc">
+                    <font style="" size="4">[<a href="https://github.com/TheDrRobert" style="">GitHUB</a></font>
+                    <a href="https://github.com/TheDrRobert">
+                    </a>
+
+                </font>
+            </div>
+            <div style="text-align: center;">
+
+                <font color="#cccccc">
+                    <a href="https://www.vicious1.com/members/stabilizinghumanity/">
+                        <font size="4">Vicious1</font>
+                    </a>
+
+                </font>
+            </div>
+            <div style="text-align: center;">
+
+                <font color="#cccccc">
+                    <a href="https://grabcad.com/robert.dillard-2">
+                        <font size="4">GrabCAD</font>
+                    </a>
+
+                </font>
+            </div>
+            <div style="text-align: center;">
+
+                <font color="#cccccc">
+                    <a href="https://www.thingiverse.com/The_Dr_Robert/about">
+                        <font size="4">Thingiverse</font>
+                    </a>
+
+                </font>
+            </div>
+            <div style="text-align: center;">
+                <font color="#cccccc">
+                    <a href="https://gallery.autodesk.com/fusion360/users/5M5APMXXQMP5" style="">
+
+                        <font style="" size="4">Fusion360</font>
+                    </a>]</font>
+                <a href="https://gallery.autodesk.com/fusion360/users/5M5APMXXQMP5"></a>
+            </div>
+            <div style="text-align: center;">
+                <font size="5" color="#f1c232" style="">Donations/Support:</font>
+            </div>
+            <div style="text-align: center;">
+
+                <font size="4" color="#cccccc">[<a href="//paypal.me/DrRobert" style="">PayPal</a>]</font>
+            </div>
+            <font size="4" color="#0000ff">
+            </font>
+        </div>
+    </div>
+    <div data-silex-type="html" class="editable-style silex-id-1507392459628-0 html-element silex-component silex-component-form silex-use-height-not-minheight" data-silex-id="silex-id-1507392459628-0" style="">
+        <div class="silex-element-content">
+            <form id="id_1507392583412_133" action="https://formspree.io/robertdwow@gmail.com" method="POST">
+
+                <div>
+                    <label for="field1">Name</label>
+                    <input type="text" required="" id="field1" name="field1" placeholder="Your name">
+                </div>
+
+
+                <div>
+                    <label for="field2">Your email adress</label>
+                    <input type="text" required="" id="field2" name="field2" placeholder="you@email.com">
+                </div>
+
+
+                <div class="fill-vertical">
+                    <label for="field3">Your message</label>
+                    <textarea required="" id="field3" name="field3" placeholder="What do you have to say?"></textarea>
+                </div>
+
+                <input type="submit" value="Send">
+            </form>
+            <style>
+                #id_1507392583412_133 input[type=text],
+                select {
+                    width: 100%;
+                    padding: 12px 20px;
+                    margin: 8px 0;
+                    display: inline-block;
+                    border: 1px solid #ccc;
+                    border-radius: 3px;
+                    box-sizing: border-box;
+                }
+                
+                #id_1507392583412_133 input[type=submit] {
+                    width: 100%;
+                    background-color: #4CAF50;
+                    color: #FFFFFF;
+                    padding: 14px 20px;
+                    margin: 8px 0;
+                    border: 1px solid #4CAF50;
+                    border-radius: 3px;
+                    cursor: pointer;
+                }
+                
+                #id_1507392583412_133 label {
+                    color: #c0c0c0;
+                }
+                /* *************************** */
+                /* resize the 3rd field
+   and fill the vertical space */
+                
+                #id_1507392583412_133 .fill-vertical {
+                    flex: 1 1 auto;
+                    display: flex;
+                    flex-direction: column;
+                }
+                
+                #id_1507392583412_133 .fill-vertical textarea {
+                    flex: 1 1 auto;
+                    width: 100%;
+                    height: 150px;
+                    padding: 12px 20px;
+                    box-sizing: border-box;
+                    border: 2px solid #ccc;
+                    border-radius: 3px;
+                    background-color: #f8f8f8;
+                    font-size: 16px;
+                    resize: none;
+                }
+                /* ************************ */
+                
+                #id_1507392583412_133 {
+                    border-radius: 3px;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                    height: 100%;
+                }
+            </style>
+
+        </div>
+    </div>
+</body>
+
+</html>
